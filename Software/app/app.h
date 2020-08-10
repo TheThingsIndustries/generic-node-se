722a53f61821a62304fa464d96038385ff1c88d8
ahmed <elsalahy92@gmail.com>
2020-08-10 16:40:01 +0200

    app: Add accelerometer basic read and restructured function calls
    all: Update with minor astyle fixes

--- Software/app/app.h
+++ Software/app/app.h
@@ -31,6 +31,7 @@
 #include "stm32_adv_trace.h"
 #include "SHTC3.h"
 #include "MX25R16.h"
+#include "LIS2DH12.h"
 
 #define APP_PPRINTF(...)  do{ } while( UTIL_ADV_TRACE_OK != UTIL_ADV_TRACE_COND_FSend(VLEVEL_OFF, T_REG_OFF, TS_OFF, __VA_ARGS__) ) //Polling Mode
 #define APP_TPRINTF(...)   do{ {UTIL_ADV_TRACE_COND_FSend(VLEVEL_OFF, T_REG_OFF, TS_ON, __VA_ARGS__);} }while(0); //with timestamp
