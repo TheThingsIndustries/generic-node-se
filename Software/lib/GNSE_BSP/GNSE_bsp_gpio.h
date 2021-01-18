edd3759e21d5236ec538d41de4d5ecea10e77036
ahmed <elsalahy92@gmail.com>
2021-01-18 17:35:08 +0100

    lib: Remove BSP support for V0.1 boards

--- Software/lib/GNSE_BSP/GNSE_bsp_gpio.h
+++ Software/lib/GNSE_BSP/GNSE_bsp_gpio.h
@@ -30,130 +30,6 @@
 /**
  * HW aliases for the board components
  */
-#if (GNSE_BSP_VERSION == GNSE_BSP_V_0_1)
-typedef enum
-{
-  LED1 = 0,
-  LED2 = 1,
-  /* Color LED aliases */
-  LED_RED = LED1,
-  LED_BLUE = LED2
-} Led_TypeDef;
-
-typedef enum
-{
-  BUTTON_SW1 = 0,
-} Button_TypeDef;
-
-typedef enum
-{
-  BUTTON_MODE_GPIO = 0,
-  BUTTON_MODE_EXTI = 1
-} ButtonMode_TypeDef;
-
-typedef enum
-{
-  LOAD_SWITCH1 = 0,
-  LOAD_SWITCH2 = 1,
-  LOAD_SWITCH3 = 2,
-  /* Load Switch aliases */
-  LOAD_SWITCH_SENSORS = LOAD_SWITCH1,
-  LOAD_SWITCH_FLASH = LOAD_SWITCH2,
-  LOAD_SWITCH_SECURE_ELEMENT = LOAD_SWITCH3
-} Load_Switch_TypeDef;
-
-
-/**
- * HAL defines
- * Configure below for any pin change
- */
-
-#define LEDn 2
-
-#define LED1_PIN GPIO_PIN_13
-#define LED1_GPIO_PORT GPIOC
-#define LED1_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
-#define LED1_GPIO_CLK_DISABLE() __HAL_RCC_GPIOC_CLK_DISABLE()
-
-#define LED2_PIN GPIO_PIN_5
-#define LED2_GPIO_PORT GPIOB
-#define LED2_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LED2_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define LEDx_GPIO_CLK_ENABLE(__INDEX__) \
-  do                                    \
-  {                                     \
-    if ((__INDEX__) == LED1)            \
-      LED1_GPIO_CLK_ENABLE();           \
-    else if ((__INDEX__) == LED2)       \
-      LED2_GPIO_CLK_ENABLE();           \
-  } while (0)
-
-#define LEDx_GPIO_CLK_DISABLE(__INDEX__) \
-  do                                     \
-  {                                      \
-    if ((__INDEX__) == LED1)             \
-      LED1_GPIO_CLK_DISABLE();           \
-    else if ((__INDEX__) == LED2)        \
-      LED2_GPIO_CLK_DISABLE();           \
-  } while (0)
-
-#define BUTTONn 1
-
-#define BUTTON_SW1_PIN GPIO_PIN_3
-#define BUTTON_SW1_GPIO_PORT GPIOB
-#define BUTTON_SW1_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define BUTTON_SW1_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-#define BUTTON_SW1_EXTI_LINE EXTI_LINE_3
-#ifdef CORE_CM0PLUS
-#define BUTTON_SW1_EXTI_IRQn EXTI3_0_IRQn
-#else
-#define BUTTON_SW1_EXTI_IRQn EXTI3_IRQn
-#endif
-
-#define BUTTONx_GPIO_CLK_ENABLE(__INDEX__) BUTTON_SW1_GPIO_CLK_ENABLE();
-
-#define LOAD_SWITCHn 3
-
-#define LOAD_SWITCHx_GPIO_CLK_ENABLE(__INDEX__) __HAL_RCC_GPIOB_CLK_ENABLE()   /* All load switches on same port */
-#define LOAD_SWITCHx_GPIO_CLK_DISABLE(__INDEX__) __HAL_RCC_GPIOB_CLK_DISABLE() /* All load switches on same port */
-
-#define LOAD_SWITCH1_PIN GPIO_PIN_12
-#define LOAD_SWITCH1_GPIO_PORT GPIOB
-#define LOAD_SWITCH1_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LOAD_SWITCH1_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define LOAD_SWITCH2_PIN GPIO_PIN_7
-#define LOAD_SWITCH2_GPIO_PORT GPIOB
-#define LOAD_SWITCH2_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LOAD_SWITCH2_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define LOAD_SWITCH3_PIN GPIO_PIN_6
-#define LOAD_SWITCH3_GPIO_PORT GPIOB
-#define LOAD_SWITCH3_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LOAD_SWITCH3_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define VBAT_READ_PIN GPIO_PIN_4
-#define VBAT_READ_PORT GPIOB
-#define VBAT_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOB_CLK_ENABLE()
-#define VBAT_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define VBAT_ADC_PIN GPIO_PIN_2
-#define VBAT_ADC_PORT GPIOB
-#define VBAT_ADC_CHANNEL_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOB_CLK_ENABLE()
-#define VBAT_ADC_CHANNEL_GPIO_CLK_DISABLE() __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define VBAT_ADC                             ADC
-#define VBAT_ADC_RES                         ADC_RESOLUTION_12B
-#define VBAT_ADC_CHANNEL                     ADC_CHANNEL_4
-#define VREF_ADC_CHANNEL                     ADC_CHANNEL_VREFINT
-#define VBAT_ADC_CLK_ENABLE()               __HAL_RCC_ADC_CLK_ENABLE()
-#define VBAT_ADC_CLK_DISABLE()              __HAL_RCC_ADC_CLK_DISABLE()
-#define VBAT_ADC_FORCE_RESET()              __HAL_RCC_ADC_FORCE_RESET()
-#define VBAT_ADC_RELEASE_RESET()            __HAL_RCC_ADC_RELEASE_RESET()
-
-#elif (GNSE_BSP_VERSION == GNSE_BSP_V_0_2)
-
 typedef enum
 {
   LED1 = 0,
@@ -183,9 +59,7 @@ typedef enum
   /* Load Switch aliases */
   LOAD_SWITCH_SENSORS = LOAD_SWITCH1,
   LOAD_SWITCH_FLASH = LOAD_SWITCH2,
-  LOAD_SWITCH_SECURE_ELEMENT = LOAD_SWITCH1 //TODO: Remove after testing, see https://github.com/TheThingsIndustries/generic-node-se/issues/74
 } Load_Switch_TypeDef;
-
 
 /**
  * HAL defines
@@ -294,7 +168,6 @@ typedef enum
 #define VBAT_ADC_CLK_DISABLE()              __HAL_RCC_ADC_CLK_DISABLE()
 #define VBAT_ADC_FORCE_RESET()              __HAL_RCC_ADC_FORCE_RESET()
 #define VBAT_ADC_RELEASE_RESET()            __HAL_RCC_ADC_RELEASE_RESET()
-#endif
 
 /**
  * BSP GPIO APIs
