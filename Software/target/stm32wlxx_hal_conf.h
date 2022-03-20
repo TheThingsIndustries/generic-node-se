893916e85f6c6af8896498bfbef4aa47e9b499e7
Alvaro <alvieboy@alvie.com>
2022-03-20 13:00:41 +0000

    HAL: use HW 128 divider for LSI (generates input for IWDG)

--- Software/target/stm32wlxx_hal_conf.h
+++ Software/target/stm32wlxx_hal_conf.h
@@ -125,7 +125,7 @@
   * @brief Internal Low Speed oscillator (LSI) value.
   */
 #if !defined (LSI_VALUE)
-#define LSI_VALUE                           32000UL     /*!< LSI Typical Value in Hz*/
+#define LSI_VALUE                           (32000UL/128UL)     /*!< LSI Typical Value in Hz*/
 #endif /* LSI_VALUE */                                  /*!< Value of the Internal Low Speed oscillator in Hz
                                                         The real value may vary depending on the variations
                                                         in voltage and temperature. */
